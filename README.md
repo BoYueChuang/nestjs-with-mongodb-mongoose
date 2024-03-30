e25ca0586db2c0e9505d225f52d61567df6175fb
BoYueChuang <otis29516368@gmail.com>
2024-03-30 16:08:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# express-vercel
+# echo "# express_mongodb" >> README.md
+# sudo git add .
+# sudo git commit -m "first commit"
+# sudo push -u origin master
---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(), // 加載環境變數
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule], // 注入ConfigModule以使用ConfigService
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_DB_CONNECTION_STRING'),
+      }),
+      inject: [ConfigService], // 注入ConfigService
+    }),
+    UsersModule,
+    PostsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
